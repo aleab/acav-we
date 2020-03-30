ab28416f819008e4ed78086acc24013b53b82217
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2020-03-30 23:31:08 +0200

    Finished navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { NavLink, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import React, { useReducer, useRef } from 'react';
+import { Link, NavLink, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import Octicon, { MarkGithub, ThreeBars } from '@primer/octicons-react';
 
 // Components
 import Home from './pages/Home';
@@ -12,16 +13,25 @@ import 'milligram';
 import './app.css';
 
 export default function App() {
+    const toggleRef = useRef<HTMLButtonElement>(null);
+    const [ isNavActive, toggleIsNavActive ] = useReducer((prev: boolean) => !prev, false);
+
+    const navbarNavClass = !isNavActive || (toggleRef.current?.offsetWidth ?? 0) <= 0 ? 'navbar-nav' : 'navbar-nav active';
+
     return (
       <Router>
         <header className="navbar">
           <div className="container">
-            <div className="navbar-item">
-              <nav className="navbar-nav">
-                <NavLink exact to="/" className="nav-item nav-link" activeClassName="active">Home</NavLink>
-                <NavLink to="/token" className="nav-item nav-link" activeClassName="active">Token</NavLink>
-              </nav>
-            </div>
+            <button ref={toggleRef} type="button" className="button-link navbar-toggle" onClick={toggleIsNavActive}>
+              <Octicon icon={ThreeBars} width={24} height={24} verticalAlign="middle" ariaLabel="Toggle Menu" />
+            </button>
+            <Link to="/" className="navbar-title">aCAV-WE</Link>
+            <a href="//github.com/aleab/acav-we" rel="external" className="navbar-link order-1" style={{ display: 'flex' }}>
+              <Octicon icon={MarkGithub} width={22} height={22} verticalAlign="middle" ariaLabel="View on Github" />
+            </a>
+            <nav className={`${navbarNavClass} ml-80-auto order-2 order-80-none`}>
+              <NavLink to="/token" className="nav-item nav-link" activeClassName="active">Token</NavLink>
+            </nav>
           </div>
         </header>
         <main className="container">
