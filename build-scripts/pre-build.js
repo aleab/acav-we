335a7e74c80f733de23f99583376418da869f252
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2020-04-02 15:49:54 +0200

    Fixed minor issue with pre-build script

--- build-scripts/pre-build.js
+++ build-scripts/pre-build.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
 const buildProjectJson = require('../project.json/build');
 
+const DIST_DIR = path.resolve('./dist');
+
 (async () => {
+    if (!fs.existsSync(DIST_DIR)) fs.mkdirSync(DIST_DIR);
     await buildProjectJson().catch(e => {
         console.error(e);
     });
