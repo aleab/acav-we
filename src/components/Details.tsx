c0885143328b9b199f0ea034639bfa638e40ac15
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2021-03-21 04:35:39 +0100

    Fixed alignment of <details> and its content

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,17 +1,31 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 
-import React, { useCallback } from 'react';
+import React, { useCallback, useMemo, useRef } from 'react';
 
-export default function Details({ children }: any) {
+interface DetailsProps {
+    toggleCallback?: (t: HTMLElement, open: boolean) => void;
+    children?: any;
+}
+
+export default function Details(props: DetailsProps) {
+    const toggleCallback = useMemo(() => props.toggleCallback, [props.toggleCallback]);
+    const ref = useRef<HTMLElement>(null);
+
     const onMouseDown = useCallback((event: React.MouseEvent<HTMLElement, MouseEvent>) => {
         // Prevent focus on click
         event.preventDefault();
     }, []);
 
+    const onToggle = useCallback((event: React.SyntheticEvent<HTMLElement, Event>) => {
+        if (ref.current) {
+            toggleCallback?.(ref.current, ref.current.hasAttribute('open'));
+        }
+    }, [toggleCallback]);
+
     return (
-      <details onMouseDown={onMouseDown}>
-        {children}
+      <details ref={ref} onMouseDown={onMouseDown} onToggle={onToggle}>
+        {props.children}
       </details>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 
-import React from 'react';
+import React, { useCallback, useReducer, useRef } from 'react';
 
 import Details from '../components/Details';
 
@@ -9,6 +9,22 @@ export default function Home() {
       <p>Your browser doesn&apos;t support HTML5 video. Here is a <a href={href}>link to the video</a> instead.</p>
     );
 
+    const openDetail = useRef<HTMLElement>();
+    const [ _currentlyOpenDetail, setCurrentlyOpenDetail ] = useReducer((_current: HTMLElement | undefined, value: HTMLElement | undefined) => {
+        openDetail.current = value;
+        return value;
+    }, undefined);
+
+    const onDetailsToggle = useCallback((t: HTMLElement, open: boolean) => {
+        if (open) {
+            if (Object.is(openDetail.current, t)) return;
+            openDetail.current?.removeAttribute('open');
+            setCurrentlyOpenDetail(t);
+        } else if (Object.is(openDetail.current, t)) {
+            setCurrentlyOpenDetail(undefined);
+        }
+    }, []);
+
     return (
       <>
         <h2 className="lead fw-4">aCAV-WE</h2>
@@ -16,29 +32,37 @@ export default function Home() {
           <p> A highly customizable audio visualizer for <a href="https://www.wallpaperengine.io/">Wallpaper Engine</a>.</p>
         </blockquote>
 
-        <h3 className="lead">Features</h3>
-        <Details>
-          <summary>Extensive customization options</summary>
-          <video width="864" controls autoPlay={false}>
-            <source src="./media/showcase.mp4" type="video/mp4" />
-            <VideoAlt href="./media/showcase.mp4" />
-          </video>
-        </Details>
-        <Details>
-          <summary>Spotify integration</summary>
-          <video width="864" controls autoPlay={false}>
-            <source src="./media/spotify.mp4" type="video/mp4" />
-            <VideoAlt href="./media/spotify.mp4" />
-          </video>
-        </Details>
-        <Details>
-          <summary>iCUE support for RGB hardware</summary>
-          <video width="864" controls autoPlay={false}>
-            <source src="./media/icue.mp4" type="video/mp4" />
-            <VideoAlt href="./media/icue.mp4" />
-          </video>
-          <p><a href="https://www.jamendo.com/track/1719234/skyline">Skiline (2020)</a> by <a href="https://www.jamendo.com/artist/484695/samie-bower">Samie Bower</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">CC BY-NC-ND</a>.</p>
-        </Details>
+        <section id="features">
+          <h3 className="lead">Features</h3>
+          <Details toggleCallback={onDetailsToggle}>
+            <summary>Extensive customization options</summary>
+            <div>
+              <video width="864" controls autoPlay={false}>
+                <source src="./media/showcase.mp4" type="video/mp4" />
+                <VideoAlt href="./media/showcase.mp4" />
+              </video>
+            </div>
+          </Details>
+          <Details toggleCallback={onDetailsToggle}>
+            <summary>Spotify integration</summary>
+            <div>
+              <video width="864" controls autoPlay={false}>
+                <source src="./media/spotify.mp4" type="video/mp4" />
+                <VideoAlt href="./media/spotify.mp4" />
+              </video>
+            </div>
+          </Details>
+          <Details toggleCallback={onDetailsToggle}>
+            <summary>iCUE support for RGB hardware</summary>
+            <div>
+              <video width="864" controls autoPlay={false}>
+                <source src="./media/icue.mp4" type="video/mp4" />
+                <VideoAlt href="./media/icue.mp4" />
+              </video>
+              <p><a href="https://www.jamendo.com/track/1719234/skyline">Skiline (2020)</a> by <a href="https://www.jamendo.com/artist/484695/samie-bower">Samie Bower</a> is licensed under <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">CC BY-NC-ND</a>.</p>
+            </div>
+          </Details>
+        </section>
       </>
     );
 }
