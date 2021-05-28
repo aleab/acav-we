b4b271f2e8c541aadda0205a1d1839e83d6df7db
Alessandro Attard Barbini <11708634+aleab@users.noreply.github.com>
2021-05-28 16:19:16 +0200

    Fixed Sphere geometry's theta angles

--- src/components/visualizers/3d/ParametricRenderers/Sphere.ts
+++ src/components/visualizers/3d/ParametricRenderers/Sphere.ts
@@ -15,28 +15,28 @@ export default class Sphere extends ParametricGeometryRenderer<SphereParameters>
         params: SphereParameters,
         initMaterial?: (material: Material, i: number, isLeft: boolean) => void,
     ) {
-        const dθ = Math.PI / nSamples;
+        const dθ = Math.PI_2 / nSamples;
         for (let i = 0; i < nSamples; ++i) {
             // LEFT
             {
-                const geometry = new SphereGeometry(params.radius, segments, segments, 0, 2 * Math.PI, Math.PI - (i + 1) * dθ, dθ);
+                const geometry = new SphereGeometry(params.radius, segments, segments, 0, 2 * Math.PI, Math.PI_2 - (i + 1) * dθ, dθ);
                 const material = new MeshLambertMaterial({ color: 0x000000, emissive: 0x000000, reflectivity: 0.25, dithering: true });
+                geometry.rotateZ(Math.PI_2);
                 initMaterial?.(material, i, true);
 
                 const mesh = new Mesh(geometry, material);
                 mesh.position.set(position.x, position.y, position.z);
-                mesh.rotateZ(Math.PI_2);
                 leftMeshes.push(mesh);
             }
             // RIGHT
             {
-                const geometry = new SphereGeometry(params.radius, segments, segments, 0, 2 * Math.PI, Math.PI + i * dθ, dθ);
+                const geometry = new SphereGeometry(params.radius, segments, segments, 0, 2 * Math.PI, Math.PI_2 + i * dθ, dθ);
                 const material = new MeshLambertMaterial({ color: 0x000000, emissive: 0x000000, reflectivity: 0.25, dithering: true });
+                geometry.rotateZ(Math.PI_2);
                 initMaterial?.(material, i, false);
 
                 const mesh = new Mesh(geometry, material);
                 mesh.position.set(position.x, position.y, position.z);
-                mesh.rotateZ(Math.PI_2);
                 rightMeshes.push(mesh);
             }
         }
